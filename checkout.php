ce5e776a811a2b24f90b149853ca61ff99138173
achillsetara16 <150452024+achillsetara16@users.noreply.github.com>
2023-12-15 19:18:12 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,13 +1,13 @@
 <?php
-    session_start();
+session_start();
 
-    if (!isset($_SESSION["user"])){
-        header("Location:login.php");
-        exit;
-    }
-    $user = $_SESSION['user']; 
+if (!isset($_SESSION["user"])) {
+    header("Location:login.php");
+    exit;
+}
+$user = $_SESSION['user'];
 
-    if (isset($_GET['logout'])) {
+if (isset($_GET['logout'])) {
 
     session_destroy();
 
@@ -28,89 +28,89 @@
     <link rel="stylesheet" href="style.css">
 
     <style>
-    #carouselExampleDark {
-        height: 450px;
-        width: 1200px;
-        margin: auto;
-        border-radius: 200px;
-    }
+        #carouselExampleDark {
+            height: 450px;
+            width: 1200px;
+            margin: auto;
+            border-radius: 200px;
+        }
 
-    .carousel-inner {
-        height: 450px;
-        width: 1200px;
-    }
+        .carousel-inner {
+            height: 450px;
+            width: 1200px;
+        }
 
-    .jarak-botton {
-        margin-top: -10px;
-    }
+        .jarak-botton {
+            margin-top: -10px;
+        }
 
-    #customNumber {
-        width: 50px;
+        #customNumber {
+            width: 50px;
 
-    }
+        }
 
-    html {
-        height: 100%;
-        width: 100%;
-    }
+        html {
+            height: 100%;
+            width: 100%;
+        }
 
-    .countainer {
-        display: flex;
-        justify-content: center;
+        .countainer {
+            display: flex;
+            justify-content: center;
 
-    }
+        }
 
-    .bottontambah {
-        width: 20px;
-        height: 20px;
-        background-color: #D9D9D9;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-        margin: 0 px
-    }
+        .bottontambah {
+            width: 20px;
+            height: 20px;
+            background-color: #D9D9D9;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            margin: 0 px
+        }
 
-    .bottonkurang {
-        width: 20px;
-        height: 20px;
-        background-color: #D9D9D9;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-        margin: 0 px
-    }
+        .bottonkurang {
+            width: 20px;
+            height: 20px;
+            background-color: #D9D9D9;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            margin: 0 px
+        }
 
-    #angka {
-        width: 20px;
-        height: 20px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+        #angka {
+            width: 20px;
+            height: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
 
-    .angka {
-        width: 20px;
-        height: 20px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin: 0 5px
-    }
+        .angka {
+            width: 20px;
+            height: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0 5px
+        }
 
 
-    .bottontambah p {
-        text-align: center;
-        margin: 0;
+        .bottontambah p {
+            text-align: center;
+            margin: 0;
 
-    }
+        }
 
-    .bottonkurang p {
-        text-align: center;
-        margin: 0;
+        .bottonkurang p {
+            text-align: center;
+            margin: 0;
 
-    }
+        }
     </style>
 </head>
 
@@ -147,7 +147,9 @@
                     <i class="fa-solid fa-user me-3 text-dark"></i>
                 </a>
                 <a href="" style="text-decoration: none;">
-                    <small class="me-5 mt text-dark"><b><?php echo $user;?></b></small>
+                    <small class="me-5 mt text-dark"><b>
+                            <?php echo $user; ?>
+                        </b></small>
                 </a>
                 <a href="?logout"><i class="fa-solid fa-sign-out-alt me-5 text-danger"></i></a>
             </div>
@@ -158,14 +160,14 @@
 
 
     <?php
-include 'koneksi.php';
-$id_barang = $_GET['id_barang'];
+    include 'koneksi.php';
+    $id_barang = $_GET['id_barang'];
 
-$result = mysqli_query($koneksi, "SELECT * FROM keranjang WHERE id_barang='$id_barang'");
-$data = mysqli_fetch_array($result)
+    $result = mysqli_query($koneksi, "SELECT * FROM keranjang WHERE id_barang='$id_barang'");
+    $data = mysqli_fetch_array($result)
 
-   
-?>
+
+        ?>
     <form action="" method="post">
         <div class="countainer-fluid col-md-10 mx-auto"
             style="margin: 70px 30px; border: 50px; background-color:#faf6f6;box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);">
@@ -175,7 +177,9 @@ $data = mysqli_fetch_array($result)
                 </center>
                 <div class="row">
                     <div class="col-md-3 mt-1 mb-3 me-1">
-                        <b>Penjual : <span id="getNamaPenjual"> <?php echo $data['username_penjual'];?></span></b>
+                        <b>Penjual : <span id="getNamaPenjual">
+                                <?php echo $data['username_penjual']; ?>
+                            </span></b>
                     </div>
                 </div>
                 <div class="row">
@@ -191,11 +195,13 @@ $data = mysqli_fetch_array($result)
                 </div>
                 <div class=" row" style="border-bottom: solid 1px black;">
                     <div class="col-md-1 " style="margin-left:50px;">
-                        <img src="file/<?php echo $data['foto_barang'];?>" alt="" class="w-75 h-75 me-2">
-                        <input type="hidden" name="" id="getFoto" value="<?php echo $data['foto_barang'];?>">
+                        <img src="file/<?php echo $data['foto_barang']; ?>" alt="" class="w-75 h-75 me-2">
+                        <input type="hidden" name="" id="getFoto" value="<?php echo $data['foto_barang']; ?>">
                     </div>
                     <div class="col-md-5 me-5">
-                        <p id="getNamaBarang"> <?php echo $data['nama_barang']; ?></p>
+                        <p id="getNamaBarang">
+                            <?php echo $data['nama_barang']; ?>
+                        </p>
                     </div>
                     <div class="col-md-2 mb-2">
                         <div class="d-flex align-items-center">
@@ -211,8 +217,9 @@ $data = mysqli_fetch_array($result)
                         </div>
                     </div>
                     <div class="col-md-3 ">
-                        <p style="color: #FF7F09;">Rp.<span class="harga" data-harga="10"
-                                id="getHargaBarang"><?php echo $data['harga_barang'];?></span></p>
+                        <p style="color: #FF7F09;">Rp.<span class="harga" data-harga="10" id="getHargaBarang">
+                                <?php echo $data['harga_barang']; ?>
+                            </span></p>
                     </div>
                 </div>
                 <div class="row">
@@ -235,16 +242,18 @@ $data = mysqli_fetch_array($result)
                     </div>
                 </div>
                 <?php
-              include 'koneksi.php';
-              $result = mysqli_query($koneksi, "SELECT * FROM login_pembeli ");
-              $dataAlamat = mysqli_fetch_array($result)
-              ?>
+                include 'koneksi.php';
+                $result = mysqli_query($koneksi, "SELECT * FROM login_pembeli ");
+                $dataAlamat = mysqli_fetch_array($result)
+                    ?>
                 <div class=" row" style="border-bottom: solid 1px black;">
                     <div class="col-md-4 me-5" style="margin-left:50px;">
-                        <p id="getAlamat"><?php echo $dataAlamat['alamat'];?></p>
+                        <p id="getAlamat">
+                            <?php echo $dataAlamat['alamat']; ?>
+                        </p>
                     </div>
                     <div class=" col-md-2 ">
-                        <a href="#" data-toggle="modal" data-target="#editalamat<?php echo $dataAlamat['username'];?>"
+                        <a href="#" data-toggle="modal" data-target="#editalamat<?php echo $dataAlamat['username']; ?>"
                             class="text-decoration-none " style="color:#FF7F09;">Ubah Alamat</a>
                     </div>
 
@@ -297,20 +306,21 @@ $data = mysqli_fetch_array($result)
                 </div>
                 <div class="row">
                     <div class="col-md-11 mt-3 mb-3 d-flex justify-content-end " style="color:#FF7F09;">
-                        <p class="me-5" id="gettotalTransaksi">Rp.<span
-                                class="total"><?php echo $data['harga_barang'];?></span></p>
+                        <p class="me-5" id="gettotalTransaksi">Rp.<span class="total">
+                                <?php echo $data['harga_barang']; ?>
+                            </span></p>
 
                         <input type="hidden" name="nama_barang" id="hiddenNamaBarang"
-                            value="<?php echo $data['nama_barang'];?>">
+                            value="<?php echo $data['nama_barang']; ?>">
                         <input type="hidden" name="harga_barang" id="hiddenHargaBarang" value="">
                         <input type="hidden" name="jumlah_barang" id="hiddenJumlahBarang" value="">
                         <input type="hidden" name="alamat" id="hiddenAlamat"
-                            value="<?php echo $dataAlamat['alamat'];?>">
+                            value="<?php echo $dataAlamat['alamat']; ?>">
                         <input type="hidden" name="status_transaksi" id="" value="Menunggu Konfirmasi">
-                        <input type="hidden" name="foto" id="hiddenFoto" value="<?php echo $data['foto_barang'];?>">
+                        <input type="hidden" name="foto" id="hiddenFoto" value="<?php echo $data['foto_barang']; ?>">
                         <input type="hidden" name="ongkos_kirim" id="ongkos_kirim" value="">
                         <input type="hidden" name="nama_penjual" id="nama_penjual"
-                            value="<?php echo $data['username_penjual'];?>">
+                            value="<?php echo $data['username_penjual']; ?>">
                         <input type="hidden" name="total_transaksi" id="total_transaksi" value="">
                         <button type="submit" name="keranjang" class="btn btn-primary pt-2">Bayar</button>
 
@@ -326,120 +336,120 @@ $data = mysqli_fetch_array($result)
 
 
     <script>
-    // manipulasi input checout
+        // manipulasi input checout
 
-    const jumlahBarang = document.getElementById('hiddenJumlahBarang')
-    const totalHargaBarang = document.getElementById('hiddenHargaBarang')
-    const alamat = document.getElementById('hiddenAlamat')
-    const getJumlahBarang = document.getElementById('getJumlahBarang')
-    const inputOngkosPengiriman = document.getElementById('ongkos_kirim')
-    const total_transaksi = document.getElementById('total_transaksi')
-    const gettotalTransaksi = document.getElementById('gettotalTransaksi')
-    const angka = document.querySelector(".angka");
-
-
-    // manipulasi total harga ke inputHiden
-    const tambahButtons = document.querySelector(".bottontambah");
-    const kurangButtons = document.querySelector(".bottonkurang");
-    const hargaElement = document.querySelector(".harga");
-    const ongkos = document.querySelector(".ongkos");
-    const total = document.querySelector(".total");
-
-    // ubah int
-    const harga = parseFloat(hargaElement.innerText.replace(/\./g, ''), 10);
-    const ongkosPengiriman = parseFloat(ongkos.innerText.replace(/\./g, ''), 10);
-    const totalBiaya = harga + ongkosPengiriman;
+        const jumlahBarang = document.getElementById('hiddenJumlahBarang')
+        const totalHargaBarang = document.getElementById('hiddenHargaBarang')
+        const alamat = document.getElementById('hiddenAlamat')
+        const getJumlahBarang = document.getElementById('getJumlahBarang')
+        const inputOngkosPengiriman = document.getElementById('ongkos_kirim')
+        const total_transaksi = document.getElementById('total_transaksi')
+        const gettotalTransaksi = document.getElementById('gettotalTransaksi')
+        const angka = document.querySelector(".angka");
 
 
-    // ubh strg
-    inputOngkosPengiriman.value = ongkosPengiriman.toLocaleString('id-ID');
-    total.innerText = totalBiaya.toLocaleString('id-ID');
-    let i = 1;
-    let hargaValue = parseInt(hargaElement.innerHTML.replace(/\./g, ''), 10);
+        // manipulasi total harga ke inputHiden
+        const tambahButtons = document.querySelector(".bottontambah");
+        const kurangButtons = document.querySelector(".bottonkurang");
+        const hargaElement = document.querySelector(".harga");
+        const ongkos = document.querySelector(".ongkos");
+        const total = document.querySelector(".total");
+
+        // ubah int
+        const harga = parseFloat(hargaElement.innerText.replace(/\./g, ''), 10);
+        const ongkosPengiriman = parseFloat(ongkos.innerText.replace(/\./g, ''), 10);
+        const totalBiaya = harga + ongkosPengiriman;
 
 
-    function tambah() {
-        tambahButtons.addEventListener("click", function() {
-            i++;
-            angka.innerHTML = i;
-            hargaElement.innerHTML = (i * hargaValue).toLocaleString('id-ID');
-            // total byr setelah btn tambah
-            const jmlHarga = parseFloat(hargaElement.innerText.replace(/\./g, ''), 10);
-            total.innerText = (jmlHarga + ongkosPengiriman).toLocaleString('id-ID');
-            jumlahBarang.value = angka.innerText
-            totalHargaBarang.value = jmlHarga.toLocaleString('id-ID');
-            total_transaksi.value = gettotalTransaksi.innerText;
+        // ubh strg
+        inputOngkosPengiriman.value = ongkosPengiriman.toLocaleString('id-ID');
+        total.innerText = totalBiaya.toLocaleString('id-ID');
+        let i = 1;
+        let hargaValue = parseInt(hargaElement.innerHTML.replace(/\./g, ''), 10);
 
-        });
-    }
 
-    function kurang() {
-        kurangButtons.addEventListener("click", function() {
-            if (i > 1) {
-                i--;
+        function tambah() {
+            tambahButtons.addEventListener("click", function () {
+                i++;
                 angka.innerHTML = i;
                 hargaElement.innerHTML = (i * hargaValue).toLocaleString('id-ID');
-
-                // total byr setelah btn kurang
+                // total byr setelah btn tambah
                 const jmlHarga = parseFloat(hargaElement.innerText.replace(/\./g, ''), 10);
                 total.innerText = (jmlHarga + ongkosPengiriman).toLocaleString('id-ID');
                 jumlahBarang.value = angka.innerText
                 totalHargaBarang.value = jmlHarga.toLocaleString('id-ID');
-
-                // aksi total harga
                 total_transaksi.value = gettotalTransaksi.innerText;
-               
 
-            }
-        });
-    }
+            });
+        }
 
-    tambah();
-    kurang();
+        function kurang() {
+            kurangButtons.addEventListener("click", function () {
+                if (i > 1) {
+                    i--;
+                    angka.innerHTML = i;
+                    hargaElement.innerHTML = (i * hargaValue).toLocaleString('id-ID');
+
+                    // total byr setelah btn kurang
+                    const jmlHarga = parseFloat(hargaElement.innerText.replace(/\./g, ''), 10);
+                    total.innerText = (jmlHarga + ongkosPengiriman).toLocaleString('id-ID');
+                    jumlahBarang.value = angka.innerText
+                    totalHargaBarang.value = jmlHarga.toLocaleString('id-ID');
+
+                    // aksi total harga
+                    total_transaksi.value = gettotalTransaksi.innerText;
+
+
+                }
+            });
+        }
+
+        tambah();
+        kurang();
     </script>
     <?php
 
-        include "koneksi.php";
+    include "koneksi.php";
 
-        date_default_timezone_set("Asia/Jakarta");
+    date_default_timezone_set("Asia/Jakarta");
 
-        if(isset($_POST["keranjang"])){
+    if (isset($_POST["keranjang"])) {
 
-            $namaBarang = $_POST['nama_barang'];
-            $harga_barang = $_POST['harga_barang'];
-            $jumlahBarang = $_POST['jumlah_barang'];
-            $alamat = $_POST['alamat'];
-            $status = $_POST['status_transaksi'];
-            $penjual = $_POST['nama_penjual'];
-            $bank = $_POST['bank'];
-            $kurir = $_POST['kurir'];
-            $ongkosKirim = $_POST['ongkos_kirim'];
-            $totalTransaksi = $_POST['total_transaksi'];
-            $foto = $_POST['foto'];
-            $tanggal = date(" d/m/Y H:i:s");
-            $user = $_SESSION["user"];
-            $query = "INSERT INTO data_transaksi (nama_barang, username_pembeli, total_harga, jumlah, alamat, tanggal_transaksi, kurir, ongkos_kirim, nama_penjual, status_transaksi, bank, foto_barang, total_transaksi) VALUES ('$namaBarang', '$user', '$harga_barang','$jumlahBarang','$alamat','$tanggal', '$kurir', '$ongkosKirim','$penjual','$status', '$bank', '$foto', '$totalTransaksi')";
-            $result = mysqli_query($koneksi, $query);
-            
-            
-            if ($result) {
-                
-            } else {
-                die(mysqli_error($koneksi));
-            }
-            
+        $namaBarang = $_POST['nama_barang'];
+        $harga_barang = $_POST['harga_barang'];
+        $jumlahBarang = $_POST['jumlah_barang'];
+        $alamat = $_POST['alamat'];
+        $status = $_POST['status_transaksi'];
+        $penjual = $_POST['nama_penjual'];
+        $bank = $_POST['bank'];
+        $kurir = $_POST['kurir'];
+        $ongkosKirim = $_POST['ongkos_kirim'];
+        $totalTransaksi = $_POST['total_transaksi'];
+        $foto = $_POST['foto'];
+        $tanggal = date(" d/m/Y H:i:s");
+        $user = $_SESSION["user"];
+        $query = "INSERT INTO data_transaksi (nama_barang, username_pembeli, total_harga, jumlah, alamat, tanggal_transaksi, kurir, ongkos_kirim, nama_penjual, status_transaksi, bank, foto_barang, total_transaksi) VALUES ('$namaBarang', '$user', '$harga_barang','$jumlahBarang','$alamat','$tanggal', '$kurir', '$ongkosKirim','$penjual','$status', '$bank', '$foto', '$totalTransaksi')";
+        $result = mysqli_query($koneksi, $query);
+
+
+        if ($result) {
+
+        } else {
+            die(mysqli_error($koneksi));
         }
-        
+
+    }
+
     ?>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="js/bootstrap.min.js"></script>
 </body>
 
